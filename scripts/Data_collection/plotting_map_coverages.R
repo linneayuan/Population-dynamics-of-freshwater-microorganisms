738fd0c04d2b2d9477927f646d1da66259e763d9
Your Name <linnea@Linneas-Air-5.bredband2.com>
2022-01-05 11:34:57 +0100

    legend scale changed

--- scripts/Data_collection/plotting_map_coverages.R
+++ scripts/Data_collection/plotting_map_coverages.R
@@ -4,20 +4,20 @@ library(mapview)
 pal = mapviewPalette("mapviewTopoColors")
 
 #Choose file based on organism, these contain avg_depth and perc_cov
-filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_FspIIIbA1.csv'
+filename <- 'data/geo_cov_FspIIIbA1.csv'
 
-filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_FspIIIbA2.csv'
+filename <- 'data/geo_cov_FspIIIbA2.csv'
 
-filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_Fubi.csv'
+filename <- 'data/depth_files_with_all_info/geo_cov_Fubi.csv'
 
-filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_Muni.csv'
+filename <- 'data/geo_cov_Muni.csv'
 
-filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_Nabu.csv'
-filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_Pver.csv'
-filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_Pcos.csv'
-filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_Psp-MWH-Mekk-B1.csv'
+filename <- 'data/depth_files_with_all_info/geo_cov_Nabu.csv'
+filename <- 'data/geo_cov_Pver.csv'
+filename <- 'data/depth_files_with_all_info/geo_cov_Pcos.csv'
+filename <- 'data/geo_cov_Psp-MWH-Mekk-B1.csv'
 
-samples <- read.csv(filename, sep=",")
+samples <- read.csv(filename, sep=";")
 
 # Transform into spatial object
 # coords - set the second columns in samples to longitude and latitude
@@ -29,8 +29,8 @@ samples_sf <- st_as_sf(samples, coords = c("Longitude", "Latitude"),  crs = 4326
 
 # Map spatial object on interactive map by average depth
 depth_thresh_2 <- samples_sf[samples_sf$avg_depth>=2,] #threshold 2
-Nabu <- depth_thresh_2[depth_thresh_2$perc_cov>=50,] #threshhold perc_cov>=50
-mapview(Nabu, zcol=c("avg_depth"), legend="TRUE")
+Psp_MWH_Mekk_B1 <- depth_thresh_2[depth_thresh_2$perc_cov>=50,] #threshhold perc_cov>=50
+mapview(Psp_MWH_Mekk_B1, at=seq(2,122,10), zcol=c("avg_depth"), legend="TRUE")
 
 
 
