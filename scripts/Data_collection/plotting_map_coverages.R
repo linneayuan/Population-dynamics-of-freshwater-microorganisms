6cfabde23b9c296a9f5bb0389efc1d818ba7af3e
chels0 <chelso97@gmail.com>
2022-01-05 09:53:12 +0100

    filters on %cov now also

--- scripts/Data_collection/plotting_map_coverages.R
+++ scripts/Data_collection/plotting_map_coverages.R
@@ -4,21 +4,18 @@ library(mapview)
 pal = mapviewPalette("mapviewTopoColors")
 
 #Choose file based on organism, these contain avg_depth and perc_cov
-filename <- 'data/depth_files_with_all_info/geo_cov_FspIIIbA1.csv'
+filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_FspIIIbA1.csv'
 
-filename <- 'data/depth_files_with_all_info/geo_cov_FspIIIbA2.csv'
+filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_FspIIIbA2.csv'
 
-filename <- 'data/depth_files_with_all_info/geo_cov_Fubi.csv'
+filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_Fubi.csv'
 
-filename <- 'data/depth_files_with_all_info/geo_cov_Muni.csv'
+filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_Muni.csv'
 
-filename <- 'data/depth_files_with_all_info/geo_cov_Nabu.csv'
-
-filename <- 'data/depth_files_with_all_info/geo_cov_Psp-MWH-Mekk-B1.csv'
-
-filename <- 'data/depth_files_with_all_info/geo_cov_Pver.csv'
-
-filename <- 'data/depth_files_with_all_info/geo_cov_Pcos.csv'
+filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_Nabu.csv'
+filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_Pver.csv'
+filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_Pcos.csv'
+filename <- '/home/chelsea/Dokument/X5/Slutkursen/geo_cov_directories/366/geo_cov_Psp-MWH-Mekk-B1.csv'
 
 samples <- read.csv(filename, sep=",")
 
@@ -28,11 +25,12 @@ samples <- read.csv(filename, sep=",")
 samples_sf <- st_as_sf(samples, coords = c("Longitude", "Latitude"),  crs = 4326)
 
 # Map spatial object on interactive map by coverage percentages
-mapview(samples_sf, zcol=c("perc_cov"), legend="TRUE")
+#mapview(samples_sf, zcol=c("perc_cov"), legend="TRUE")
 
 # Map spatial object on interactive map by average depth
 depth_thresh_2 <- samples_sf[samples_sf$avg_depth>=2,] #threshold 2
-mapview(depth_thresh_2, zcol=c("avg_depth"), legend="TRUE")
+Nabu <- depth_thresh_2[depth_thresh_2$perc_cov>=50,] #threshhold perc_cov>=50
+mapview(Nabu, zcol=c("avg_depth"), legend="TRUE")
 
 
 
