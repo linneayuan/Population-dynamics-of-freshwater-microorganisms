b9d738e5788259d94efcec1efad95b20c2580dfd
nathaniel-b-b <81922755+nathaniel-b-b@users.noreply.github.com>
2022-01-10 10:28:56 +0100

    Update MatrixPlot.R

--- scripts/Analysis/MatrixPlot.R
+++ scripts/Analysis/MatrixPlot.R
@@ -1,11 +1,11 @@
-max(coord)
-max(all)
-min(coord)
-min(all)
-plot(coord[1, 1], all[1, 1], xlim=c(0, 12500), ylim=c(0,0.6), xlab="Distance (km)", ylab="Fst value", main="Density scatter plot (Muni)")
+max(coord) #Get max
+max(all) #Get max
+min(coord) #Get min
+min(all) #Get min
+plot(coord[1, 1], all[1, 1], xlim=c(0, 12500), ylim=c(0,0.6), xlab="Distance (km)", ylab="Fst value", main="Density scatter plot (Muni)") #Edit manually; values obtained above goes into xlim and ylim
 for(i in 2:110){
 for(k in 1:110){
-points(coord[i, k], all[i, k])}}
-for(i in 2:110){
-points(coord[1,i], all[1,i])
+points(coord[i, k], all[i, k])}} #Plot all points onto the plot, except the first row
+for(i in 1:110){
+points(coord[1,i], all[1,i]) #Plot all points onto the plot from the first row
 }
